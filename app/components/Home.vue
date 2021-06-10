8fe69be7aadfecf90bf1967c387648e876b59d48
PROehidna <31016747+PROehidna@users.noreply.github.com>
2021-06-10 18:45:22 +0500

    first commit

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -6,30 +6,30 @@
             <Label  class="input1" row="1" col="0" colSpan="4" v-model="ev"  />
             <Label  class="input2" row="2" col="0" colSpan="4" v-model="result" />
 
-            <Button class="btn5" row="3" col="0" text="(" @tap="val('(')" />
-            <Button class="btn5" row="3" col="1" text=")" @tap="val(')')" />
+            <Button class="btnBkt" row="3" col="0" text="(" @tap="val('(')" />
+            <Button class="btnBkt" row="3" col="1" text=")" @tap="val(')')" />
 
-            <Button class="btn2" row="4" col="0" text="C" @tap="cls" />    
-            <Button v-if="ev" class="btn2" row="4" col="1" text="<--" @tap="clrlast" />
-            <Button class="btn2" row="4" col="2" text="/" @tap="val('/')" />
-            <Button class="btn2" row="4" col="3" text="*" @tap="val('*')" />
+            <Button class="btnGreen" row="4" col="0" text="C" @tap="cls" />    
+            <Button v-if="ev" class="btnGreen" row="4" col="1" text="<--" @tap="clrlast" /> <!-- Условная отрисовка -->
+            <Button class="btnGreen" row="4" col="2" text="/" @tap="val('/')" />
+            <Button class="btnGreen" row="4" col="3" text="*" @tap="val('*')" />
 
             <Button class="btn" row="5" col="0" text="7" @tap="val('7')" />
             <Button class="btn" row="5" col="1" text="8" @tap="val('8')"/>
             <Button class="btn" row="5" col="2" text="9" @tap="val('9')" />
-            <Button class="btn2" row="5" col="3" text="-" @tap="val('-')" />
+            <Button class="btnGreen" row="5" col="3" text="-" @tap="val('-')" />
 
             <Button class="btn" row="6" col="0" text="4" @tap="val('4')" />
             <Button class="btn" row="6" col="1" text="5" @tap="val('5')" />
             <Button class="btn" row="6" col="2" text="6" @tap="val('6')" />
-            <Button class="btn2" row="6" col="3" text="+" @tap="val('+')" />
+            <Button class="btnGreen" row="6" col="3" text="+" @tap="val('+')" />
 
             <Button class="btn" row="7" col="0" text="1" @tap="val('1')" />
             <Button class="btn" row="7" col="1" text="2" @tap="val('2')" />
             <Button class="btn" row="7" col="2" text="3" @tap="val('3')" />
-            <Button class="btn2 btn3" row="7" col="3" text="=" rowSpan="2" @tap="calcit" />/>
+            <Button class="btnGreen btnResult" row="7" col="3" text="=" rowSpan="2" @tap="calcit" />/>
 
-            <Button class="btn btn4" row="8" col="0" text="0" colSpan="2" @tap="val('0')" />/>
+            <Button class="btn btnZero" row="8" col="0" text="0" colSpan="2" @tap="val('0')" />/>
             <Button class="btn" row="8" col="2" text="." @tap="val('.')" />/>
 
         </GridLayout>
@@ -69,7 +69,7 @@
 </script>
 
 <style scoped>
-.btn, .btn2, .btn3, .btn4, .btn5 {
+.btn, .btnGreen, .btnResult, .btnZero, .btnBkt {
     background-color: #6699ff;
     color: #ffffff;
     font-size: 24;
@@ -78,16 +78,16 @@
     border: 0;
     border-radius: 40;
 }
-.btn2 {
+.btnGreen {
     background-color: #009900;
 }
-.btn3 {
+.btnResult {
     height: 180;
 }
-.btn4 {
+.btnZero {
     width: 180;
 }
-.btn5 {
+.btnBkt {
     font-size: 15;
     width: 80;
     height: 40;
